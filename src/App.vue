0918f11a22e28fca3c5ff0aa1716791da43000a4
JohnatanLucas <41579475+JohnatanLucas@users.noreply.github.com>
2020-02-01 01:04:33 -0300

    Commit 1º configuração do Header

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,62 @@
 <template>
-	<v-app>
-		<Header />
-		<v-content>
-			<v-container>
-				<transition name="slide" mode="out-in">
-					<router-view></router-view>
-				</transition>
-			</v-container>
-		</v-content>
-	</v-app>
+  <v-app>
+    <div class="div-principal">
+      <Header />
+      <router-view></router-view>
+    </div>
+  </v-app>
 </template>
 
 <script>
-import Header from './components/Header'
-
+import Header from "./components/Header";
 
 export default {
-	components: { Header },
-	created() {
-	
-	}
-}
+  components: { Header },
+  created() {}
+};
 </script>
 
 <style>
-	@keyframes slide-in {
-		from { transform: translateY(-30px); opacity: 0; }
-		to { transform: translateY(0px); opacity: 1; }
-	}
+* {
+  font-family: sans-serif;
+  font-style: normal;
+  font-weight: normal;
+}
 
-	@keyframes slide-out {
-		from { transform: translateY(0px); opacity: 1; }
-		to { transform: translateY(-30px); opacity: 0; }
-	}
+.div-principal {
+  height: 100%;
+  background-image: url(images/wallpaper.png);
+  background-repeat: no-repeat;
+  background-size: 100%;
+}
 
-	.slide-enter-active {
-		animation: slide-in 0.3s ease;
-	}
+@keyframes slide-in {
+  from {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0px);
+    opacity: 1;
+  }
+}
 
-	.slide-leave-active {
-		animation: slide-out 0.3s ease;
-	}
+@keyframes slide-out {
+  from {
+    transform: translateY(0px);
+    opacity: 1;
+  }
+  to {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+}
+
+.slide-enter-active {
+  animation: slide-in 0.3s ease;
+}
+
+.slide-leave-active {
+  animation: slide-out 0.3s ease;
+}
 </style>
-
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,25 +1,119 @@
 
 <template>
   <h1>MENU PRINCIPAL</h1>
+    <div class="header">
+      <div class="icon">
+        <img class="icon-logo" src="../images/logo.svg" />
+        <span class="text1">clube de consultas</span>
+      </div>
+      <div class="localization">
+        <div class="icon-localization">
+          <img class src="../images/localization.png" />
+        </div>
+        <div class="text-localization">
+          <span class="text2">Palhoça - SC</span>
+          <span class="text3">Essa não é minha localização</span>
+        </div>
+      </div>
+      <div class="login">
+        <b-button class="btn-principal">Cadastrar-se</b-button>
+        <span class="btn-login">Login</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
 </script>
 
 <style>
-.myButton {
-/* Rectangle 8 */
+.header {
+  display: flex;
+  justify-content: space-around;
+  height: 90px;
+  width: 100%;
+}
 
+.icon {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
 
-position: absolute;
-width: 150px;
-height: 51px;
-left: 1091px;
-top: 17.6px;
+.icon-logo {
+  width: 100%;
+  height: 100%;
+}
+.localization {
+  display: flex;
+  align-items: center;
+  height: 100%;
+  width: 20%;
+}
 
-background: #E5695A;
-border-radius: 60px;
+.text-localization {
+  display: flex;
+  flex-direction: column;
+  height: 45%;
+  width: 45%;
+  padding-left: 2%;
+}
+.login {
+  display: flex;
+  align-items: center;
+  display: flex;
+  height: 100%;
+}
+
+.btn-principal {
+  display: flex;
+  width: 150px;
+  height: 51px;
+  left: 1091px;
+  top: 17.6px;
+  justify-content: center;
+  background: #e5695a;
+  border-radius: 60px;
+}
+
+.btn-login {
+  display: flex;
+  width: 44px;
+  height: 23px;
+  left: 1271px;
+  top: 29.6px;
+  margin-left: 20%;
+  font-size: 18px;
+  line-height: 21px;
+
+  color: #e5695a;
+}
+
+.text1 {
+  font-family: Asap;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 21px;
+  color: #99cdcd;
+}
+.text2 {
+  /* Palhoça - SC */
+  font-family: Asap;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  color: #069999;
+}
+.text3 {
+  /* Essa não é minha localização */
+  font-family: Asap;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 14px;
+  text-decoration-line: underline;
+  color: #99cdcd;
 }
 </style>
 
