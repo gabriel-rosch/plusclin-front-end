c475a642eb969c5b7209d125dd6ae6ec2fe5a889
JohnatanLucas <41579475+JohnatanLucas@users.noreply.github.com>
2020-07-22 22:50:13 -0300

    Update

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var port = process.env.PORT || 8080;
 var app = express();
 
 app.use(express.static(__dirname + "/dist/"));
-app.get(/.*/,function (req,res) {
-res.sendFile(__dirname + "/dist/index.html");
+app.get(/.*/, function (req, res) {
+    res.sendfile(__dirname + "/dist/index.html");
 });
 
 app.listen(port);
