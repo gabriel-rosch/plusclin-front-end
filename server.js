88c08ece56a40c82b9545106754dc5739ceee2ad
JohnatanLucas <41579475+JohnatanLucas@users.noreply.github.com>
2020-07-22 22:41:12 -0300

    Revert "Com erro"
    This reverts commit cf40fac8332a8b33f29867899ac324a76b65e552.

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express');
 var port = process.env.PORT || 8080;
 var app = express();
 
-app.use(express.static(__dirname + "/distt/"));
+app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/,function (req,res) {
-res.sendFile(__dirname + "/distt/index.html");
+res.sendFile(__dirname + "/dist/index.html");
 });
 
 app.listen(port);
