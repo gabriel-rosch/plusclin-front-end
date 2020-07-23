363b14edef15ce5f893c479a861ee945dfa1a6aa
JohnatanLucas <41579475+JohnatanLucas@users.noreply.github.com>
2020-07-23 00:19:11 -0300

    Update Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -59,7 +59,6 @@
                 </button>
             </b-row>
         </div>
-        teste2
     </main>
 <!--                <div class="flex-column box-information">-->
 <!--                    <div class="d-flex box-one">-->
