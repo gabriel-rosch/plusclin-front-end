cf40fac8332a8b33f29867899ac324a76b65e552
JohnatanLucas <41579475+JohnatanLucas@users.noreply.github.com>
2020-07-22 22:39:00 -0300

    Com erro

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express');
 var port = process.env.PORT || 8080;
 var app = express();
 
-app.use(express.static(__dirname + "/dist/"));
+app.use(express.static(__dirname + "/distt/"));
 app.get(/.*/,function (req,res) {
-res.sendFile(__dirname + "/dist/index.html");
+res.sendFile(__dirname + "/distt/index.html");
 });
 
 app.listen(port);
