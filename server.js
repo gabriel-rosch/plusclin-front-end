250796f0ae3e808f6970c8315b49c2219c209e32
JohnatanLucas <41579475+JohnatanLucas@users.noreply.github.com>
2020-07-22 22:30:35 -0300

    Configuração server.js
    Configuração para deploy

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var port = process.env.PORT || 8080;
+var app = express();
+
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/,function (req,res) {
+res.sendFile(__dirname + "/dist/index.html");
+});
+
+app.listen(port);
+
+console.log("Teste");
+
